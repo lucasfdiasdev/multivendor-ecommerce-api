f516c7f14a898521542a676bcb702518593f7fcb
Lucas Dias <lucasfdiasdev@gmail.com>
2024-05-20 17:14:35 -0300

    feat: create seller register controller and route

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,10 +1,11 @@
-import bcrypt from "bcryptjs";
 import { NextFunction, Request, Response } from "express";
 
 import { sendToken } from "../utils/jwt";
 import { ErrorHandler } from "../utils/error-handler";
 import { userAdminModel } from "../models/admin.model";
+import { userSellerModel } from "../models/seller.model";
 import { CatchAsyncError } from "../middleware/catch-async-error";
+import { sellerCostumerModel } from "../models/chat/seller-customer.model";
 
 interface IAdminLoginRequest {
   email: string;
@@ -35,6 +36,47 @@ export const admin_login = CatchAsyncError(
       }
 
       sendToken(adminUser, 200, res);
+    } catch (error: any) {
+      return next(new ErrorHandler(500, error.message));
+    }
+  }
+);
+
+interface ISellerRegisterRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export const sellerRegister = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { name, email, password } = req.body as ISellerRegisterRequest;
+
+      if (!name || !email || !password) {
+        return next(
+          new ErrorHandler(400, "Please provide name, email and password")
+        );
+      }
+
+      const existUser = await userSellerModel.findOne({ email });
+      if (existUser) {
+        return next(new ErrorHandler(400, "Usuário já cadastrado."));
+      }
+
+      const newSeller = await userSellerModel.create({
+        name,
+        email,
+        password,
+        method: "menualy",
+        shopInfo: {},
+      });
+
+      await sellerCostumerModel.create({
+        myId: newSeller.id,
+      });
+
+      sendToken(newSeller, 201, res);
     } catch (error: any) {
       return next(new ErrorHandler(500, error.message));
     }
--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,6 +1,10 @@
 import express from "express";
-import { admin_login } from "../controllers/auth.controller";
+
+import { admin_login, sellerRegister } from "../controllers/auth.controller";
 
 export const authRouter = express.Router();
 
 authRouter.post("/admin-login", admin_login);
+
+// sellers
+authRouter.post("/seller-register", sellerRegister);
