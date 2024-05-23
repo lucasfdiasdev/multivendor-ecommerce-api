fe0a1e19efb0cc5c33097d77c15d568354d6479e
Lucas Dias <lucasfdiasdev@gmail.com>
2024-05-23 04:43:29 -0300

    rafactor: added data query in the seller model

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,15 +1,17 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
 
-import { IUserAdmin } from "../models/admin.model";
 import { ErrorHandler } from "../utils/error-handler";
 import { CatchAsyncError } from "./catch-async-error";
+import { IUserSeller, userSellerModel } from "../models/seller.model";
+import { IUserAdmin, userAdminModel } from "../models/admin.model";
 
 // declare global
 declare global {
   namespace Express {
     interface Request {
       userAdmin?: IUserAdmin;
+      userSeller?: IUserSeller;
     }
   }
 }
@@ -32,17 +34,35 @@ export const isAuthenticated = CatchAsyncError(
       return next(new ErrorHandler(400, "access token is not valid"));
     }
 
+    // Busca o usuário no banco de dados admin
+    const user = await userAdminModel.findById(decoded.id);
+
+    // Se não encontrar no modelo de administrador, busca no modelo de vendedor
+    if (!user) {
+      const userSeller = await userSellerModel.findById(decoded.id);
+
+      if (!userSeller) {
+        return next(new ErrorHandler(400, "User is not authenticated"));
+      }
+
+      req.userSeller = userSeller;
+    } else {
+      req.userAdmin = user;
+    }
+
     next();
   }
 );
 
 export const authorizeRoles = (...roles: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    if (!roles.includes(req.userAdmin?.role || "")) {
+    const userRole = req.userAdmin?.role || req.userSeller?.role;
+
+    if (!roles.includes(userRole || "")) {
       return next(
         new ErrorHandler(
           403,
-          `Role: ${req.userAdmin?.role} is not allowed to access this resource`
+          `Role: ${userRole} is not allowed to access this resource`
         )
       );
     }
